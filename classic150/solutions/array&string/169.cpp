23308d3ce98a736be65db0e4a0b4e195a613db0e
OscarTian <141600688+OscarTian@users.noreply.github.com>
2026-07-14 23:52:41 +0800

    Update 169.cpp

--- classic150/solutions/array&string/169.cpp
+++ classic150/solutions/array&string/169.cpp
@@ -19,3 +19,23 @@ public:
     }
 };
 
+/*
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int vote = 1, ans = nums[0];
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != ans) {
+                vote--;
+                if(vote == 0) {
+                    ans = nums[i];
+                    vote = 1;
+                }
+            } else {
+                vote++;
+            }
+        }
+        return ans;
+    }
+};
+*/
