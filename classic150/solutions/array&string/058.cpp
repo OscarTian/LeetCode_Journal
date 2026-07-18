b0d44e6d6ac64355a50de0a45903156b2bb4a647
OscarTian <141600688+OscarTian@users.noreply.github.com>
2026-07-18 10:35:50 +0800

    Create 058.cpp

---
+++ classic150/solutions/array&string/058.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int end = s.size() - 1;
+        while (end >= 0 && s[end] == ' ') end--;
+        int len = 0;
+        while (end >= 0 && s[end] != ' ') {
+            len++;
+            end--;
+        }
+        return len;
+    }
+};
