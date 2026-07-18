eb39f21d736f8d1a00b3bbb64895b6da972595c5
OscarTian <141600688+OscarTian@users.noreply.github.com>
2026-07-18 16:45:57 +0800

    Update 028.cpp

--- classic150/solutions/array&string/028.cpp
+++ classic150/solutions/array&string/028.cpp
@@ -26,3 +26,25 @@ public:
     }
 };
 */
+
+/* 算法最优解：KMP
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int h = haystack.size(), n = needle.size();
+        if (n == 0) return 0;
+        vector<int> next(n, 0);
+        for (int i = 1, j = 0; i < n; ++i) {
+            while (j > 0 && needle[i] != needle[j]) j = next[j - 1];
+            if (needle[i] == needle[j]) ++j;
+            next[i] = j;
+        }
+        for (int i = 0, j = 0; i < h; ++i) {
+            while (j > 0 && haystack[i] != needle[j]) j = next[j - 1];
+            if (haystack[i] == needle[j]) ++j;
+            if (j == n) return i - n + 1;
+        }
+        return -1;
+    }
+};
+*/
