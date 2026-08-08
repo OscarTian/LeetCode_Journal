7be2c425190a4156f80827781eec5bcaed6faf8d
OscarTian <141600688+OscarTian@users.noreply.github.com>
2026-08-08 11:29:11 +0800

    Update 035.cpp

---
+++ classic150/solutions/binary-search/035.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0;
+        int right=nums.size()-1;
+        int mid;
+        while(left<right)
+        {
+            mid=(left+right)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]>target)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        if(target>nums[left])
+            return left+1;
+        return left;
+    }
+};
