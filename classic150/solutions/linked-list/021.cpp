819103498e1865d9df297cdd41ed380afbf9733b
OscarTian <141600688+OscarTian@users.noreply.github.com>
2026-08-13 11:56:27 +0800

    Create 021.cpp

---
+++ classic150/solutions/linked-list/021.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1)
+            return list2;
+        if(!list2)
+            return list1;
+        ListNode a(0);
+        ListNode* p=&a;
+        while(list1&&list2)
+        {
+            if(list1->val<=list2->val)
+            {
+                p->next=list1;
+                p=p->next;
+                list1=list1->next;
+            }
+            else
+            {
+                p->next=list2;
+                p=p->next;
+                list2=list2->next;
+            }
+        }
+        if(list1)
+            p->next=list1;
+        else
+            p->next=list2;
+        return a.next;
+    }
+};
