2ecf8be488942824ac37a214db003c3eb7bf3035
OscarTian <141600688+OscarTian@users.noreply.github.com>
2026-08-13 18:53:26 +0800

    Create 191.cpp

---
+++ classic150/solutions/bit-manipulation/191.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans=0;
+        while(n>=1)
+        {
+            ans+=n%2;
+            n=n/2;
+        }
+        return ans;
+    }
+};
