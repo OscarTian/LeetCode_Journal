3545fbd54cc5c9088181ffa3389785d1cf0d3777
OscarTian <141600688+OscarTian@users.noreply.github.com>
2026-08-13 11:35:03 +0800

    Create 205.cpp

---
+++ classic150/solutions/hash-table/205.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> ss=track(s);
+        vector<int> tt=track(t);
+        for(int i=0;i<ss.size();i++)
+            if(ss[i]!=tt[i])
+                return false;
+        return true;
+    }
+    vector<int> track(string s)
+    {
+        int signal=1;
+        vector<int> signe(128,0);
+        vector<int> ss;
+        for(int i=0;i<s.size();i++)
+        {
+            if(signe[s[i]]==0)
+            {
+                signe[s[i]]=signal;
+                ss.push_back(signal);
+                signal++;
+            }
+            else
+                ss.push_back(signe[s[i]]);
+        }
+        return ss;
+    }
+};
+
+};
